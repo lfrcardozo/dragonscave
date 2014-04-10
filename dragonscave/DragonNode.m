bc1e387410e9e5e61be5de92423ce77a1e58c6ee
lfrcardozo <lfrcardozo@MacBook-Pro-de-Luiz.local>
2014-04-10 17:02:31 -0300

    commit

--- dragonscave/DragonNode.m
+++ dragonscave/DragonNode.m
@@ -1,9 +1,9 @@
 //
 //  DragonNode.m
-//  spritybird
+//  dragonscave
 //
-//  Created by Alexis Creuzot on 16/02/2014.
-//  Copyright (c) 2014 Alexis Creuzot. All rights reserved.
+//  Created by Luiz Fernando Ramos Cardozo on 08/04/14.
+//  Copyright (c) 2014 Luiz Fernando Ramos Cardozo. All rights reserved.
 //
 
 #import "DragonNode.h"
